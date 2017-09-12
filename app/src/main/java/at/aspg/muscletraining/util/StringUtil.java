5a9b27c17ba836137ef6f4603bf9a72561b1a124
Andreas Schörgenhumer <andischoe@gmail.com>
2017-09-12 18:30:47 +0200

    added missing javadoc in StringUtil

--- app/src/main/java/at/aspg/muscletraining/util/StringUtil.java
+++ app/src/main/java/at/aspg/muscletraining/util/StringUtil.java
@@ -2,9 +2,27 @@ package at.aspg.muscletraining.util;
 
 import java.util.Iterator;
 
+/**
+ * A utility class that consists solely of static methods that can be used for operating
+ * on {@link String}s.
+ */
 public class StringUtil {
 	
-	// TODO: doc
+	/**
+	 * Creates a single String by concatenating the items in the specified Iterable using
+	 * the specified String as delimiter. On each item, the {@link Object#toString()}
+	 * method is invoked.
+	 * <p>
+	 * Given the items {A, B, ..., M, N}, the following String will be created: {@code
+	 * AxBx...MxN} where {@code x} is the specified delimiter.
+	 *
+	 * @param items     the items to concatenate into a single String using the specified
+	 *                  delimiter
+	 * @param delimiter the String to separate individual items
+	 * @param <T>       the type of the items
+	 * @return a single String by concatenating the items in the specified Iterable using
+	 * the specified delimiter
+	 */
 	public static <T> String join(Iterable<T> items, String delimiter) {
 		StringBuilder sb = new StringBuilder();
 		Iterator<T> iter = items.iterator();

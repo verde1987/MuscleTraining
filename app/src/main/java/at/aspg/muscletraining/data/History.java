f385c94aba8ac0b3200b3d13f695996f65269636
Andreas Schörgenhumer <andischoe@gmail.com>
2017-09-12 19:10:13 +0200

    added basic, preliminary interfaces and implementations for the history

--- app/src/main/java/at/aspg/muscletraining/data/History.java
+++ app/src/main/java/at/aspg/muscletraining/data/History.java
@@ -1,7 +1,54 @@
 package at.aspg.muscletraining.data;
 
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import at.aspg.muscletraining.data.plans.Plan;
+import at.aspg.muscletraining.data.plans.TrainingDay;
+
 public class History {
 	
-	// TODO: store for every plan a list of TrainingDay with the date on which the TrainingDay was exercised
+	/**
+	 * Lazily initialized, so every access must be done via {@link #getHistory()}.
+	 */
+	private static Map<Plan, List<Pair<TrainingDay, Date>>> history;
+	
+	/**
+	 * Returns the history, reading it from the file in case this method gets called for
+	 * the first time.
+	 *
+	 * @return the history
+	 */
+	public static Map<Plan, List<Pair<TrainingDay, Date>>> getHistory() {
+		if (history == null) {
+			history = readHistoryFromFile();
+		}
+		return history;
+	}
+	
+	private static Map<Plan, List<Pair<TrainingDay, Date>>> readHistoryFromFile() {
+		return null;
+	}
+	
+	public static void add(Plan plan, TrainingDay trainingDay) {
+		add(plan, trainingDay, new Date());
+	}
+	
+	public static void add(Plan plan, TrainingDay trainingDay, Date date) {
+		// TODO: parameters: requireNonNull
+		Map<Plan, List<Pair<TrainingDay, Date>>> history = getHistory();
+		List<Pair<TrainingDay, Date>> planHistory = history.get(plan);
+		if (planHistory == null) {
+			planHistory = new ArrayList<>();
+		}
+		planHistory.add(Pair.create(trainingDay, date));
+		history.put(plan, planHistory);
+		// serialize history immediately again? or sth like "newAsyncThread.invokeLater(() -> history.serialize)"
+	}
 	
 }

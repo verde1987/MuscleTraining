242c6f7c7727665804d92985cf565ecde01e8a99
Andreas Schörgenhumer <andischoe@gmail.com>
2017-09-22 23:38:04 +0200

    updated history class

--- app/src/main/java/at/aspg/muscletraining/data/History.java
+++ app/src/main/java/at/aspg/muscletraining/data/History.java
@@ -2,16 +2,24 @@ package at.aspg.muscletraining.data;
 
 import android.util.Pair;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import at.aspg.muscletraining.data.plans.Plan;
 import at.aspg.muscletraining.data.plans.TrainingDay;
+import at.aspg.muscletraining.util.IOUtil;
+import at.aspg.muscletraining.util.ObjectUtil;
 
+// TODO: make singleton? could then use "implements serializable"
 public class History {
+	
+	private static final String HISTORY_FILENAME = "history/history.xml";
 	
 	/**
 	 * Lazily initialized, so every access must be done via {@link #getHistory()}.
@@ -20,27 +28,31 @@ public class History {
 	
 	/**
 	 * Returns the history, reading it from the file in case this method gets called for
-	 * the first time.
+	 * the first time. If the history does not yet exist (e.g., in case the file was
+	 * deleted or it is the very first time this method is called), an empty history will
+	 * be returned.
 	 *
 	 * @return the history
 	 */
 	public static Map<Plan, List<Pair<TrainingDay, Date>>> getHistory() {
 		if (history == null) {
-			history = readHistoryFromFile();
+			try {
+				history = readHistoryFromFile();
+			} catch (FileNotFoundException e) {
+				history = new HashMap<>();
+			}
 		}
 		return history;
 	}
 	
-	private static Map<Plan, List<Pair<TrainingDay, Date>>> readHistoryFromFile() {
-		return null;
+	private static Map<Plan, List<Pair<TrainingDay, Date>>> readHistoryFromFile() throws FileNotFoundException {
+		File historyFile = IOUtil.getReadableInternalFile(HISTORY_FILENAME);
+		return IOUtil.deserialize(historyFile);
 	}
 	
 	public static void add(Plan plan, TrainingDay trainingDay) {
-		add(plan, trainingDay, new Date());
-	}
-	
-	public static void add(Plan plan, TrainingDay trainingDay, Date date) {
-		// TODO: parameters: requireNonNull
+		ObjectUtil.requireNonNull(plan, trainingDay);
+		Date date = new Date();
 		Map<Plan, List<Pair<TrainingDay, Date>>> history = getHistory();
 		List<Pair<TrainingDay, Date>> planHistory = history.get(plan);
 		if (planHistory == null) {
@@ -48,7 +60,20 @@ public class History {
 		}
 		planHistory.add(Pair.create(trainingDay, date));
 		history.put(plan, planHistory);
-		// serialize history immediately again? or sth like "newAsyncThread.invokeLater(() -> history.serialize)"
+		// TODO: serialize history immediately again? or sth like "newAsyncThread.invokeLater(() -> history.serialize)"
+		// TODO: append new data rather than writing the entire history!
+		// TODO: use SQLite DB? (https://developer.android.com/training/basics/data-storage/databases.html)
+		try {
+			writeHistoryToFile(history);
+		} catch (IOException e) {
+			// TODO
+			e.printStackTrace();
+		}
+	}
+	
+	private static void writeHistoryToFile(Map<Plan, List<Pair<TrainingDay, Date>>> history) throws IOException {
+		File historyFile = IOUtil.getWritableInternalFile(HISTORY_FILENAME);
+		IOUtil.serialize(history, historyFile);
 	}
 	
 }
